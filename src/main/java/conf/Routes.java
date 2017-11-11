bde7ac65a5eaf37b9d830f2e8c6829813e5ac85d
Manason <clanerp@gmail.com>
2017-11-10 16:48:43 -0800

    r button now resets the game

--- src/main/java/conf/Routes.java
+++ src/main/java/conf/Routes.java
@@ -36,6 +36,7 @@ public class Routes implements ApplicationRoutes {
         router.POST().route("/dealGame").with(ApplicationController.class, "dealPost");
         router.POST().route("/moveCard/{columnFrom}/{columnTo}").with(ApplicationController.class, "moveCard");
         router.POST().route("/removeCard/{column}").with(ApplicationController.class, "removeCard");
+        router.POST().route("/resetGame").with(ApplicationController.class, "resetGame");
 
         ///////////////////////////////////////////////////////////////////////
         // Assets (pictures / javascript)
