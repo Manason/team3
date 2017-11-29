c09e980232b55f678b4b966f8237b753f9885270
Manason <clanerp@gmail.com>
2017-11-29 00:20:49 -0800

    created SpanishDeck and added spanish suits

--- src/main/java/models/Suit.java
+++ src/main/java/models/Suit.java
@@ -3,6 +3,6 @@ package models;
 import java.io.Serializable;
 
 public enum Suit  implements Serializable {
-    Hearts, Spades, Diamonds, Clubs
+    Hearts, Spades, Diamonds, Clubs, Bastos, Oros, Copas, Espadas, Joker
 }
 
