bccd8e7d4f8a0e595c571e0b19512e0e2f7e08b7
loughrao <loughrao@oregonstate.edu>
2017-12-04 03:53:47 -0800

    Fixed tests to work with English Game

--- src/test/models/testEnglishGame.java
+++ src/test/models/testEnglishGame.java
@@ -3,19 +3,11 @@ package models;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-public class testGame {
-    @Test
-
-    public void testReset() {
-        Game g = new Game();
-        g.score = 50;
-        g.resetGame();
-        assertEquals(0, g.score);
-    }
+public class testEnglishGame {
 
     @Test
     public void testDealFour() {
-        Game g = new Game();
+        EnglishGame g = new EnglishGame();
         g.dealFour();
         assertEquals(1, g.cols.columnSize(1));
     }
